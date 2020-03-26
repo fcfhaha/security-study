43ece4b71044823b106b922a176920840dd576f0
V.Caifeng.Fan 范采鳳 <v.caifeng.fan@deltaww.com>
2020-03-26 18:14:02 +0800

    fisrt commit

---
+++ security-demo/src/main/java/com/fcf/security/demo/DemoApplication.java
@@ -0,0 +1,35 @@
+/**
+
+Delta CONFIDENTIAL
+
+
+(C) Copyright Delta Electronics, Inc. 2020 All Rights Reserved
+
+
+NOTICE:  All information contained herein is, and remains the
+property of Delta Electronics. The intellectual and technical
+concepts contained herein are proprietary to Delta Electronics
+and are protected by trade secret, patent law or copyright law.
+Dissemination of this information or reproduction of this material
+is strictly forbidden unless prior written permission is obtained
+from Delta Electronics.
+*/
+package com.fcf.security.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author V.Caifeng.Fan
+ * @ClassName DemoApplication
+ * @Description: TODO
+ * @date 2020年3月26日 下午6:08:50
+ */
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
